4034adc618747de49fb68d2fc8a64ab3cdc4c456
Tom Shea <thristhart@gmail.com>
2015-08-24 12:51:28 +0000

    Dead monster grave

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ function onReady() {
   Water.setup();
   
   Renderer.draw();
+  
+  var lastPetName = localStorage.getItem("monster_name");
+  if(lastPetName) {
+    document.getElementById("gravestone").display = "block";
+    document.getElementById("deadMonster").innerHTML = lastPetName;
+  }
 }
 
 window.addEventListener("load", onReady)
